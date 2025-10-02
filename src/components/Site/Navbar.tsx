8459238865ac213a788a97d6849a9de981ed7abc
knixan <josefineeriksson@live.se>
2025-10-02 15:05:22 +0200

    commit

--- src/components/Site/Navbar.tsx
+++ src/components/Site/Navbar.tsx
@@ -27,31 +27,31 @@ export default function Navbar() {
             <div className="ml-10 flex items-baseline space-x-8">
               <Link
                 href="/#hem"
-                className="text-white hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors"
+                className="text-white hover:text-gray-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Hem
               </Link>
               <Link
                 href="/produkter"
-                className="text-gray-400 hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors"
+                className="text-gray-400 hover:text-gray-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Produkter
               </Link>
               <Link
                 href="/julprodukter"
-                className="text-gray-400 hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors"
+                className="text-gray-400 hover:text-gray-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Julprodukter
               </Link>
               <Link
                 href="/#om-oss"
-                className="text-gray-400 hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors"
+                className="text-gray-400 hover:text-gray-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Om Oss
               </Link>
               <Link
                 href="/#kontakt"
-                className="text-gray-400 hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors"
+                className="text-gray-400 hover:text-gray-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Kontakt
               </Link>
